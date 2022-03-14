72b338a96915418cf4a85d654b2ac7af59e938fa
Kaerzetoes <kaerzetoes@student.kul.pl>
2022-03-14 18:34:56 +0100

    Changes in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello");
+        System.out.println("hello world second time");
+        System.out.println("hello world third time");
     }
 }
